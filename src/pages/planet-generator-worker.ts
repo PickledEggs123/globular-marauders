1dacd06deaab9889660c7d1da91ed49df18da81a
Tyler Truong <tyler_truong@icloud.com>
2023-11-08 16:08:47 -0500

    Added seedrandom to generate the same planet at different resolutions

--- src/pages/planet-generator-worker.ts
+++ src/pages/planet-generator-worker.ts
@@ -4,10 +4,11 @@ import {generatePlanet} from "@pickledeggs123/globular-marauders-generator/dist/
 
 // eslint-disable-next-line no-restricted-globals
 self.onmessage = (e: MessageEvent<string>) => {
+    const seed = new Date().toISOString();
 // eslint-disable-next-line no-restricted-globals
-    self.postMessage(generatePlanet(0));
+    self.postMessage(generatePlanet(0, seed));
 // eslint-disable-next-line no-restricted-globals
-    self.postMessage(generatePlanet(1));
+    self.postMessage(generatePlanet(1, seed));
 // eslint-disable-next-line no-restricted-globals
-    self.postMessage(generatePlanet(2));
+    self.postMessage(generatePlanet(2, seed));
 }
