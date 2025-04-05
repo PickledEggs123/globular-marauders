2a22a2239a1a963ea5238a230ae852809cc78e64
Tyler Truong <tyler_truong@icloud.com>
2025-04-05 00:53:55 -0400

    Added column direction to Markdown so it appears more consistent. Column direction places each paragraph in its own row so it appears lest cramped.

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,8 +1,19 @@
-import React, {ChangeEvent, useState} from "react";
-import {Avatar, Box, Button, Container, InputAdornment, Paper, Skeleton, TextField, Tooltip, Typography} from "@mui/material";
-import {useAuth} from "../contextes/auth";
+import React, { ChangeEvent, useState } from "react";
+import {
+    Avatar,
+    Box,
+    Button,
+    Container,
+    InputAdornment,
+    Paper,
+    Skeleton,
+    TextField,
+    Tooltip,
+    Typography
+} from "@mui/material";
+import { useAuth } from "../contextes/auth";
 import { WebsiteDrawer2 } from "../Drawer";
-import {model} from "../firebase/firebase";
+import { model } from "../firebase/firebase";
 import Markdown from "react-markdown";
 
 interface IChatMessage {
@@ -14,17 +25,38 @@ interface IChatMessage {
 
 const ChatBubble = (props: IChatMessage) => {
     return (
-        <Box sx={{background: props.sender ? '#cdf' : '#fff', padding: 1, margin: '8px 0px', borderRadius: 1, display: 'flex', flexDirection: props.sender ? 'row-reverse' : 'row', justifyContent: 'space-between'}}>
+        <Box
+            sx={{
+                background: props.sender ? '#cdf' : '#fff',
+                padding: 1,
+                margin: '8px 0px',
+                borderRadius: 1,
+                display: 'flex',
+                flexDirection: props.sender ? 'row-reverse' : 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                gap: '8px',
+            }}
+        >
             <Tooltip title={props.name}>
-                <Avatar sx={{width: 24, height: 24}} srcSet={props.photoURL} />
+                <Avatar sx={{ width: 24, height: 24 }} srcSet={props.photoURL} />
             </Tooltip>
-            <Markdown>{props.message}</Markdown>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    alignItems: 'flex-start',
+                }}
+            >
+                <Markdown>{props.message}</Markdown>
+            </Box>
         </Box>
     );
 };
 
 export const ChatRoom = () => {
-    const {userLoggedIn, currentUser} = useAuth();
+    const { userLoggedIn, currentUser } = useAuth();
     const [chatValue, setChatValue] = useState("");
     const [chatHistory, setChatHistory] = useState<IChatMessage[]>([]);
     const [chatDisabled, setChatDisabled] = useState<boolean>(false);
@@ -35,7 +67,7 @@ export const ChatRoom = () => {
     const handleChatChange = (event: ChangeEvent<HTMLInputElement>) => {
         event.preventDefault();
         setChatValue(event.target.value);
-    }
+    };
 
     const handleSendChat = async (e: React.SyntheticEvent) => {
         e.preventDefault();
@@ -80,46 +112,94 @@ export const ChatRoom = () => {
     };
 
     const chatRoom = userLoggedIn ? (
-        <Paper sx={{padding: 2, mt: 2}}>
-            {
-                chatHistory.map((v, i) => {
-                    return <ChatBubble
-                        key={i}
-                        sender={v.sender}
-                        name={v.name}
-                        message={v.message}
-                        photoURL={v.photoURL}
-                    />;
-                })
-            }
-            <TextField value={chatValue} disabled={chatDisabled} onChange={handleChatChange} label="Chat" fullWidth sx={{paddingTop: 1}} InputProps={{
-                startAdornment: (
-                    <InputAdornment position="start">
-                        <Tooltip title={currentUser?.displayName ?? undefined}>
-                            <Avatar sx={{width: 24, height: 24}} srcSet={currentUser?.photoURL ?? undefined}/>
-                        </Tooltip>
-                    </InputAdornment>
-                )
-            }}></TextField>
-            <Button fullWidth variant="contained" disabled={chatDisabled} onClick={handleSendChat}>Send</Button>
+        <Paper
+            sx={{
+                padding: 1,
+                marginTop: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: 1,
+            }}
+        >
+            {chatHistory.map((v, i) => (
+                <ChatBubble
+                    key={i}
+                    sender={v.sender}
+                    name={v.name}
+                    message={v.message}
+                    photoURL={v.photoURL}
+                />
+            ))}
+            <TextField
+                value={chatValue}
+                disabled={chatDisabled}
+                onChange={handleChatChange}
+                label="Chat"
+                fullWidth
+                sx={{ marginTop: 1 }}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <Tooltip title={currentUser?.displayName ?? undefined}>
+                                <Avatar sx={{ width: 24, height: 24 }} srcSet={currentUser?.photoURL ?? undefined} />
+                            </Tooltip>
+                        </InputAdornment>
+                    )
+                }}
+            />
+            <Button
+                fullWidth
+                variant="contained"
+                disabled={chatDisabled}
+                onClick={handleSendChat}
+            >
+                Send
+            </Button>
         </Paper>
     ) : (
-        <Paper sx={{padding: 2, mt: 2}}>
+        <Paper
+            sx={{
+                padding: 1,
+                marginTop: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: 1,
+            }}
+        >
             <Skeleton width="100%" height="10vh" />
             <Skeleton width="100%" height="10vh" />
             <Skeleton width="100%" height="10vh" />
-            <TextField label="Chat" fullWidth disabled sx={{paddingTop: 1}}></TextField>
-            <Typography>Please Login</Typography>
+
+            <TextField
+                label="Chat"
+                fullWidth
+                disabled
+                sx={{ marginTop: 1 }}
+            />
+
+            <Typography variant="body1">
+                Please Login
+            </Typography>
         </Paper>
     );
 
     return (
-        <Paper style={{width: "100%", minHeight: "100vh", height: "fit-content", display: "flex", flexDirection: "column"}}>
+        <Paper
+            style={{
+                width: "100%",
+                minHeight: "100vh",
+                height: "fit-content",
+                display: "flex",
+                flexDirection: "column"
+            }}
+        >
             <WebsiteDrawer2 rightSide={null} content={
                 <Container>
                     {chatRoom}
                 </Container>
-            }/>
+            } />
         </Paper>
-    )
+    );
 };
